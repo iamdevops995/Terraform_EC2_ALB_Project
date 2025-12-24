8c0ab5d19099ed5278443399e85732b3de083957
logeshkumar <iamdevops995@gmail.com>
2025-12-24 19:11:12 +0530

    [skip ci]

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "webserver1" {
   instance_type = "t2.micro"
   vpc_security_group_ids= [ aws_security_group.websg.id ]
   subnet_id = aws_subnet.sub1.id
-  user_data = base64encode(file("userdata.sh"))
+  user_data = base64encode(file("webserver01-user.sh"))
 }
 
 # EC2 Instance creation for webserver2
@@ -87,7 +87,7 @@ resource "aws_instance" "webserver2" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.websg.id ]
   subnet_id = aws_subnet.sub2.id
-  user_data = base64encode(file("userdata1.sh"))
+  user_data = base64encode(file("webserver2-user.sh"))
 }
 
 #ALB creation
